6952b166337defbdb9ad894c346d4ee25afe6ac0
WellingtonLinux <wellingtoncpfilho@gmail.com>
2023-04-10 02:43:34 -0300

    feito na gambiarra

--- src/app.js
+++ src/app.js
@@ -1,48 +1,77 @@
 import express from 'express';
 import cors from 'cors';
 
+
 const app = express();
 const PORT = 5000;
 
-const user =[];
 
-const tweets=[];
+const user = [];
+
+let trava= true;
+
+let avatari = '';
+
+const tweets = [];
+
+
 
 app.use(cors());
 app.use(express.json());
 
 
+
 app.post('/sign-up', (req, res) => {
+    
     const { username, avatar } = req.body;
+
+    if(!username || !avatar){
+     return res.sendStatus(422);
+    }
+
     const usuario = { username, avatar };
+    
     user.push(usuario);
-    res.send('OK');
+    
+    trava = false;
+    
+    avatari = avatar;
+    
+    res.sendStatus(200);
+
 });
+
 
 
 app.post('/tweets', (req, res) => {
     
-    if (user === []) {
-        res.send('UNAUTHORIZED');
-        return;
+    if (trava) {
+
+        return res.sendStatus(401);
     }
     
     const { username, tweet } = req.body;
-    const msg = { username, tweet };
 
+    if(!username || !tweet){
+        return res.sendStatus(422);
+    }
 
+    const msg = { username, tweet, avatar:avatari };
+    
+    tweets.reverse();
+    
     tweets.push(msg);
-    res.send('OK');
+
+    res.sendStatus(200);
+    
 });
 
 
 
-
-
-
-
+app.get('/tweets', (req, res) => {
+res.send(tweets.reverse().slice(0,10));
+});
 
 
 
 app.listen(PORT, () => console.log(`Conectado a porta ${PORT}`));
-
